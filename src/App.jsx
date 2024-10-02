54525072a8edd062d3674da8b2dfe4b5b6d000f1
ChenFangFangFang <sophiachan0430@gmail.com>
2024-10-02 17:15:12 +0300

    nav, tool bar completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,134 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { Link, Outlet } from 'react-router-dom'
+import { ListItemIcon } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+const drawerWidth = 200;
+const navItems = [
+  { name: 'Home', icon: <HomeIcon /> },
+  { name: 'Customer', icon: <PermIdentityIcon /> },
+  { name: 'Training', icon: <FitnessCenterIcon /> }
+];
+function DrawerAppBar(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
 
-function App() {
-  const [count, setCount] = useState(0)
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+  // Control the switch status of the sidebar (Drawer)
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant="h6" sx={{
+        my: 1,
+        backgroundColor: 'primary.main',
+        padding: '12px',
+        margin: 0,
+        color: '#ffffff',
+      }}>
+        {/* sx={{ my: 1 }}： control the padding */}
+        Personal Training
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item.name} disablePadding>
+            <ListItemButton sx={{ display: 'flex', alignItems: 'left', textAlign: 'left' }}>
+              <ListItemIcon sx={{ marginRight: '-12px', color: (theme) => theme.palette.primary.main }}>
+                {item.icon}
+              </ListItemIcon>
+              <Typography variant="body1" sx={{ color: 'inherit' }}>
+                {item.name}
+              </Typography>
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+
+    </Box >
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar component="nav">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            Personal Training Center
+          </Typography>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map((item) => (
+              <Button key={item.name} component={Link} to={`/${item.name.toLowerCase()}`} sx={{ color: '#fff' }}>
+                {item.name}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <nav>
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+      <Box component="main" sx={{ p: 3 }}>
+        <Toolbar />
+        <Outlet />
+      </Box>
+    </Box>
   )
+
 }
 
-export default App
+DrawerAppBar.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default DrawerAppBar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
+import Home from './components/Home'
+import Customer from './components/Customer';
+import Training from './components/Training';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,  // App 组件在这里被渲染
+    children: [
+      {
+        index: true,
+        path: "home",
+        element: <Home />
+      },
+      {
+        path: "customer",
+        element: <Customer />
+      },
+      {
+        path: "training",
+        element: <Training />
+      }
+    ]
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
